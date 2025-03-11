adef9d34accac25a10dcc594e2bc09ee0e937b5d
spider747344 <9630003948sri@gmail.com>
2025-03-11 20:36:14 +0530

    activity1.py

---
+++ activity1.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("Welcome to the world of programing!")
+
+#Print a Number
+print(4)
+
+#Next line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of Print Statement
+print("welcome to ",end="*")
